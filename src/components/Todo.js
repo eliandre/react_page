59e177eb07ac85ba1ceb15ab8864d104a4b525bd
Andre Eli <aeli@hot.ee>
2022-02-13 14:16:33 +0200

    Lisatud nupu jaoks eventhandler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,15 @@
 function Todo(props) {
+
+    function deleteHandler() {
+        console.log('Vajutatud!');
+        console.log(props.text);
+    }
+
     return(
         <div className="card">
         <h1>{props.text}</h1>
         <div className="actions">
-          <button className="btn">Kustuta</button>
+          <button className="btn" onClick={deleteHandler} >Kustuta</button>
         </div>
       </div>
     );
