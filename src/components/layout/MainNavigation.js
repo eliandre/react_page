32bc3051549346ed66c6af7b3a11bb063607d6bc
Andre Eli <aeli@hot.ee>
2022-02-14 22:59:03 +0200

    Lisatud stiililehed menüüle.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,22 +1,25 @@
 import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
-    return <header>
-        <div>Piimajoojate kohtumised</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Kõik kohtumised</Link>
-                </li>
-                <li>
-                    <Link to="/new-meetup">Uus kohtumine</Link>
-                </li>
-                <li>
-                    <Link to="/favorites">Lemmikud</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>Piimajoojate kohtumised</div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Kõik kohtumised</Link>
+                    </li>
+                    <li>
+                        <Link to="/new-meetup">Uus kohtumine</Link>
+                    </li>
+                    <li>
+                        <Link to="/favorites">Lemmikud</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default MainNavigation;
