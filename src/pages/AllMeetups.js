f25452dbc407dd2c8d6841a1d041317492ac4c22
Andre Eli <aeli@hot.ee>
2022-02-18 22:45:31 +0200

    Lisatud kohtumise üksus ja kohtumiste list.

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,34 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'See on esimene kohtumine',
+        image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Kohtumiste allee 5, 12345 Tartu linn',
+        description:
+            'See on esimene kohtumine. Ära maha maga!',
+    },
+    {
+        id: 'm2',
+        title: 'See on teine kohtumine',
+        image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Kohtumiste allee 5, 12345 Tartu linn',
+        description:
+            'See on teine kohtumine. Ära maha maga!',
+    },
+]
+
 function AllMeetupsPage() {
-    return <div>Kõik kohtumised</div>;
-}
+
+    return (
+        <section>
+            <h1>Kõik kohtumised</h1>
+            <MeetupList meetups={DUMMY_DATA} />
+        </section>
+    );
+};
 
 export default AllMeetupsPage;
