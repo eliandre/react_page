b616734d1a833918294f9bd1e719107c61974c75
Andre Eli <aeli@hot.ee>
2022-02-19 14:33:49 +0200

    Lisatud Firebase'i tugi ja POST meetod saatmiseks.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-page-db594-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: { 'Content-Type': 'application/json'}
+        })
+    }
+
     return(
         <section>
             <h1>Lisa uus kohtumine</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     )
 }
