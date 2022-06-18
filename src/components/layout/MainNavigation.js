d1781846d1af892f6c95e4c06da6f703917ba6c0
Andre Eli <aeli@hot.ee>
2022-06-18 23:09:36 +0300

    Lemmikute kuvamine Lemmikutesse ja badge

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
+import FavoritesContex from '../../store/favorites-contex';
 
 function MainNavigation() {
+
+    const favoritesCtx = useContext(FavoritesContex)
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>Piimajoojate kohtumised</div>
@@ -14,7 +19,10 @@ function MainNavigation() {
                         <Link to="/new-meetup">Uus kohtumine</Link>
                     </li>
                     <li>
-                        <Link to="/favorites">Lemmikud</Link>
+                        <Link to="/favorites">
+                            Lemmikud
+                            <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,24 @@
+import { useContext} from 'react';
+import FavoritesContex from '../store/favorites-contex';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage() {
-    return <div>Lemmikud</div>;
+
+    const favoritesCtx = useContext(FavoritesContex);
+
+    let content;
+
+    if(favoritesCtx.totalFavorites === 0) {
+        content = <p>Sul pole lemmikuid veel. Tahad lisada?</p>
+    }
+    else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />
+    }
+
+    return <section>
+        <h1>Minu lemmikud</h1>
+        {content}
+    </section>;
 }
 
 export default FavoritesPage;
